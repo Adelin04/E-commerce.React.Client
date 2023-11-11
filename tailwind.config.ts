c6ae8201a35404b24574f5a3a70000912559c5de
Adelin04 <adelin.marin04@yahoo.com>
2023-11-11 13:00:19 +0200

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,5 +21,10 @@ const config: Config = {
     extend: {},
   },
   plugins: [],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 export default config
