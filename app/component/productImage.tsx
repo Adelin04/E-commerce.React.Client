02839691b7e6e5cb508339f9963d54ab5cab77ad
Adelin04 <adelin.marin04@yahoo.com>
2023-11-12 11:03:22 +0200

    updated

--- app/component/productImage.tsx
+++ app/component/productImage.tsx
@@ -17,7 +17,7 @@ function ProductImage({ pathImage, width, height, alt }: PropsProductImage) {
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             {pathImage && pathImage.length > 0 ?
                 <div style={{ display: "flex", justifyContent: "center", alignContent: 'center', padding: '0px' }}>
-                    <img className={style.image} /* priority={true} */ width={width || 250} height={height || 250} src={pathImage.toString() || logoIcon} alt={'no photo'} />
+                    <Image className={style.image} priority={true} width={width || 250} height={height || 250} src={pathImage.toString() || logoIcon} alt={'no photo'} />
                 </div>
                 :
                 <div style={{ display: "flex", justifyContent: "center", alignContent: 'center', height: '100%' }}>
