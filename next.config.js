e4e30f8d709fdf00c1d0fa6d6423e7b536254f61
Adelin04 <adelin.marin04@yahoo.com>
2023-11-13 23:13:52 +0200

    updated

--- next.config.js
+++ next.config.js
@@ -2,12 +2,11 @@
 const nextConfig = {
   cssModules: true,
   reactStrictMode: false,
-  target: "serverless",
-
+  
   images: {
     domains: ["e-commerce-photos.s3.amazonaws.com"],
   },
-
+  
   env: {
     PRODUCTION_URI: "https://am-cloud.eu/api/",
     DEV_URI: "http://localhost:5050/api/",
