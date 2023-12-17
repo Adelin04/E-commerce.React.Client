ef4803ece729e2aa7290c8f8f9e6bdaa0328fd82
Adelin04 <adelin.marin04@yahoo.com>
2023-12-17 23:49:40 +0200

    updated

--- app/component/cardImages.tsx
+++ app/component/cardImages.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import React, { useState } from "react"
+import { FerrisWheelSpinner, CircleSpinnerOverlay } from 'react-spinner-overlay'
 
 interface IPropsCardImages {
     photos: []
@@ -13,15 +14,17 @@ const CardImages = ({ photos }: IPropsCardImages) => {
     const tmp_photos = ["https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg", "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg", "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg", "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w600/2023/10/free-images.jpg"]
     return (
 
-        <div className=" flex justify-center items-center min-w-max h-[300px] " >
+        <div className=" flex justify-center items-center h-[300px] " >
 
             {photos && photos.map((photo: any, index: number) => {
                 return (
                     <div className={`flex justify-center items-center h-full relative mx-2 rounded-2xl cursor-pointer`} key={index} onClick={() => setIndexActive(index)}>
-                        <Image priority className={`justify-center items-center relative h-full rounded-2xl  transition-all `} width={`${index === indexActive ? 300 : 50}`} height={300} src={photo.path} alt="Image logo" />
+
+                        {photo ? <Image priority className={`justify-center items-center relative h-full rounded-2xl  transition-all `} width={`${index === indexActive ? 300 : 50}`} height={300} src={photo.path.toString().trim()} alt="Image logo" /> : < FerrisWheelSpinner />}
                         <div className="description absolute bottom-0 right-1 flex flex-col items-end justify-center p-1 w-full">
                             <p className="flex justify-center items-center w-5 h-5 bg-black text-white rounded-full cursor-pointer" >{index + 1}</p>
                         </div>
+
                     </div>
 
                 )
